fb35fe0c8dc1b301db5ca37cbe3a56a614bda3eb
autumn <dennis456852@gmail.com>
2018-01-05 13:52:26 +0800

    edit

--- project/lib/event.js
+++ project/lib/event.js
@@ -1,11 +1,24 @@
 const web3 = require('./web3.js')
 const contract = require('./contract.js')
 
-function watchEvents(address){
+function watchEventsContribute(address){
 
     let project = new web3.eth.Contract(contract.project.abi, address);
 
     project.events.Contribute({
+        fromBlock: 0
+    })
+    .on('data', function(event){
+        console.log(event);
+    })
+    
+}
+
+function watchEventsContribute(address){
+
+    let project = new web3.eth.Expense(contract.project.abi, address);
+
+    project.events.Expense({
         fromBlock: 0
     })
     .on('data', function(event){
